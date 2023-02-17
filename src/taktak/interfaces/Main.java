9afed34d38f03674503f9eaae8254bb41b4549a1
sacre <kapungusacre01@gamil.com>
2023-02-17 22:36:53 +0100

    Second commit

---
+++ src/taktak/interfaces/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taktak.interfaces;
+
+/**
+ *
+ * @author LENOVO THINKPAD E15
+ */
+
+import taktak.services.ColisService;
+import taktak.entities.Colis;
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+       
+       Colis cls = new Colis ("abcd123", 152, 243, 42, 2, true, "Tunis", 2, 2);
+ 
+       ColisService cs = new ColisService();
+       cs.ajouterColis(cls);
+
+        
+        System.out.println(cs.afficherColis());
+        
+    }
+}
---
+++ src/taktak/services/ColisService.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taktak.services;
+import taktak.entities.Colis;
+import java.sql.*;
+import java.util.List;
+import taktak.interfaces.IColis;
+import taktak.utils.MyConnection;
+import java.util.*;
+/**
+ *
+ * @author LENOVO THINKPAD E15
+ */
+public class ColisService implements IColis{
+    
+    Connection myconn = MyConnection.getInstance().getConnexion();
+    
+    @Override
+    public void ajouterColis(Colis cls) {
+        try {
+            String sql="insert into colis values(null,?,?,?,?,?,?,?,?,?)"; 
+            PreparedStatement ste= myconn.prepareStatement(sql);
+            ste.setString(1, cls.getRef());
+            ste.setInt(2, cls.getHauteur());
+            ste.setInt(3, cls.getLargeur());
+            ste.setInt(4, cls.getPoids());
+            ste.setInt(5, cls.getPrix());
+            ste.setBoolean(6, cls.getFragile());
+            ste.setString(7, cls.getDestination());
+            ste.setInt(8, cls.getId_client());
+            ste.setInt(9, cls.getId_paiment());
+            ste.executeUpdate();
+            System.out.println("colis ajouté");
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+    
+     @Override
+    public void modifierColis(Colis cls) {
+        String sql="update colis set (?,?,?,?,?,?,?,?,?) where id= ?";
+        try {
+            PreparedStatement ste=myconn.prepareStatement(sql);
+                ste.setString(1, cls.getRef());
+                ste.setInt(2, cls.getHauteur());
+                ste.setInt(3, cls.getLargeur());
+                ste.setInt(4, cls.getPoids());
+                ste.setInt(5, cls.getPrix());
+                ste.setBoolean(6, cls.getFragile());
+                ste.setString(7, cls.getDestination());
+            ste.setInt(8, cls.getId_client());
+            ste.setInt(9, cls.getId_paiment());
+                ste.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }    
+    }
+    
+    @Override
+    public void supprimerColis(Colis cls) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    @Override
+    public List<Colis> afficherColis() {
+        List <Colis> colis = new ArrayList<>();
+        try {
+            String sql = "select * from colis";
+            Statement ste = myconn.createStatement();
+            ResultSet s = ste.executeQuery(sql);
+            while (s.next()) {
+
+                Colis cls = new Colis(s.getString("ref"),
+                        s.getInt(3),
+                        s.getInt(4),
+                        s.getInt(5),
+                        s.getInt(6),
+                        s.getBoolean(7),
+                        s.getString("destination"),
+                        s.getInt(9),
+                        s.getInt(10));
+                colis.add(cls);
+       
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return colis;
+    }  
+    
+}
