257ad1575d90d600bbb79df66908ae1e371d74e0
yasmine cherif <yasmine.cherif@esprit.tn>
2023-02-20 13:42:19 +0100

    Login and create account "Client"

---
+++ src/GUI/LoginController.java
@@ -0,0 +1,162 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package GUI;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static javafx.application.ConditionalFeature.FXML;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+import org.controlsfx.control.Notifications;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+
+/**
+ * FXML Controller class
+ *
+ * @author yasoulanda
+ */
+public class LoginController implements Initializable {
+
+    @FXML private JFXTextField username; 
+    @FXML private JFXPasswordField mdp;
+    @FXML private JFXButton loginButton;
+    @FXML private JFXButton fermer;
+       
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+loginButton.setOnAction(e -> {
+    try {
+        login(e);
+    } catch (IOException ex) {
+        ex.printStackTrace();
+    } catch (SQLException ex) {
+        Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+    }
+});
+
+
+    }    
+    
+    public void login(ActionEvent  event) throws IOException, SQLException{
+         String usernameText = username.getText();
+         String mdpText = mdp.getText();
+        
+         if(usernameText.equals("admin") && mdpText.equals("admin")){
+           Stage stage = new Stage ();
+           Parent root = FXMLLoader.load(getClass().getResource("Welcome.fxml"));  
+           Scene scene = new Scene (root);
+           stage.setScene(scene);
+           stage.initStyle(StageStyle.UNDECORATED);
+           stage.show();
+           ((Node)event.getSource()).getScene().getWindow().hide();
+           check();
+        } else {
+             if (usernameText.isEmpty()==false && mdpText.isEmpty()==false) 
+              { 
+                      Connection myconn = MyConnection.getInstance().getConnexion();
+                      String sql = "SELECT count(1) FROM client WHERE login = '"+usernameText+"' AND mdp = '"+mdpText+"'";
+                      try {
+                         Statement stmt = myconn.createStatement();
+                         ResultSet rs = stmt.executeQuery(sql);
+                         while (rs.next()) 
+                         {
+                            if( rs.getInt(1) == 1 ){
+   
+                                Stage stage = new Stage ();
+                                Parent root = FXMLLoader.load(getClass().getResource("Welcome.fxml"));  
+                                Scene scene = new Scene (root);
+                                stage.setScene(scene);
+                                stage.initStyle(StageStyle.UNDECORATED);
+                                stage.show();
+                                ((Node)event.getSource()).getScene().getWindow().hide();
+                                check();
+                            } else
+                               {
+                                   error();
+                               }
+                         }
+                                                
+                      }catch (Exception e){}
+                      
+                      
+                  
+                  
+              } 
+              else{
+                  error();
+                  }
+             }
+
+         
+        
+        
+    }
+    
+    public void error (){
+    Image image = new Image("Images/cross.png");
+     Notifications notification = Notifications.create();
+     notification.graphic(new ImageView(image));
+     notification.title("Erreur");
+     notification.text("Erreur : Nom d'utilisateur ou mot de passe incorrect ");
+     notification.hideAfter(Duration.seconds(3));
+     notification.position(Pos.CENTER);
+     notification.show();
+    }
+    
+        public void check () throws IOException{
+      String usernameText = username.getText();
+
+     Image image = new Image("Images/accept.png");
+     Notifications notification = Notifications.create();
+     notification.graphic(new ImageView(image));
+     notification.title("Succès");
+     notification.text("Bienvenue "+ usernameText );
+     notification.hideAfter(Duration.seconds(1));
+     notification.position(Pos.CENTER);
+     notification.show();
+    }
+
+        public void cancel (ActionEvent e){
+
+               Stage stage = (Stage) fermer.getScene().getWindow();
+               stage.close();
+    }
+        
+        public void creer (ActionEvent e) throws IOException{
+
+          Stage stage = new Stage ();
+          Parent root = FXMLLoader.load(getClass().getResource("Create.fxml"));  
+          Scene scene = new Scene (root);
+          stage.setScene(scene);
+          stage.initStyle(StageStyle.UNDECORATED);
+          stage.show();
+          ((Node)e.getSource()).getScene().getWindow().hide();
+    }
+        
+}
