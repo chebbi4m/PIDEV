7f3193958db34946452b6b9ca1a613fb80f4044d
mohamed chebbi <chebbim4@gmail.com>
2023-02-21 05:04:52 +0100

    added afficher in table

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -6,45 +6,46 @@
 package gui;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import taktak.entities.reclamation;
+import taktak.services.ReclamationService;
 
 
 
 
 public class Main extends Application {
     
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+       
+//       Colis cls = new Colis ("abcd123", 152, 243, 42, 2, true, "Tunis", 2, 2);
+       reclamation rec = new reclamation(3, "colis is here", "Doe Doe", "6969");
+ 
+//       ColisService cs = new ColisService();
+       ReclamationService rs = new ReclamationService();
+//       cs.ajouterColis(cls);
+
+//        rs.ajouterReclamation(rec);
+//        rs.deleteReclamation(1);
+//        rs.updateReclamation(rec);
+        System.out.println(rs.afficherReclamation());
         launch(args);
+//        System.out.println(cs.afficherColis());
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("reclamationFXML.fxml"));
+        primaryStage.setTitle("taktak");
+        primaryStage.setScene(new Scene(root, 1000, 600));
+        primaryStage.show();
     }
     
 }
